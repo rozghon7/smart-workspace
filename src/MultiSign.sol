eac9fc432b9aaca7cf6e760d7a4da45675abaada
rozghon7 <rozgonnni@gmail.com>
2025-06-26 02:31:43 +0200

    feat: Tests and new functionality added for MultiSign.sol

--- src/MultiSign.sol
+++ src/MultiSign.sol
@@ -9,6 +9,10 @@ contract MultiSign {
     uint256 public quorum;
     /// @notice The total number of transfers initiated
     uint256 public transfersCount;
+    /// @notice The current multisig signers
+    address[] public currentMultiSigSigners;
+    /// @notice The total number of signers or quorum changes
+    uint256 public signersAndQuorumChangesCount;
 
     /// @notice Constructor to initialize the contract with signers and quorum
     /// @param _signers An array of addresses that are allowed to sign transfers
@@ -21,8 +25,10 @@ contract MultiSign {
         quorum = _quorum;
 
         for (uint256 i = 0; i < _signers.length; i++) {
-            require(_signers[i] != address(0), SignerAddressCannotBeZero());
-            isSigner[_signers[i]] = true;
+            address signer = _signers[i];
+            require(signer != address(0), SignerAddressCannotBeZero());
+            currentMultiSigSigners.push(signer);
+            isSigner[signer] = true;
         }
     }
 
@@ -31,17 +37,29 @@ contract MultiSign {
     /// @param to The address to which the transfer is made
     /// @param amount The amount of the transfer
     event TransferInitiated(uint256 indexed transferId, address indexed to, uint256 amount);
-
     /// @notice Emitted when a transfer is approved by a signer
     /// @param transferId The unique identifier for the transfer
     /// @param signer The address of the signer who approved the transfer
     event TransferApproved(uint256 indexed transferId, address indexed signer);
-
     /// @notice Emitted when a transfer is executed
     /// @param transferId The unique identifier for the transfer
     /// @param to The address to which the transfer is made
     /// @param amount The amount of the transfer
     event TransferExecuted(uint256 indexed transferId, address indexed to, uint256 amount);
+    /// @notice Emitted when the multisig signers and quorum updates are initiated
+    /// @param updatesId The unique identifier for the updates
+    /// @param newSigners The new array of multisig signers
+    /// @param newQuorum The new required quorum for executing transfers
+    event MultiSigSignersAndQuorumUpdatesInitiated(uint256 indexed updatesId, address[] newSigners, uint256 newQuorum);
+    /// @notice Emitted when the multisig signers and quorum updates are approved by a signer
+    /// @param updatesId The unique identifier for the updates
+    /// @param signer The address of the signer who approved the updates
+    event MultiSigSignersAndQuorumUpdatesApproved(uint256 indexed updatesId, address indexed signer);
+    /// @notice Emitted when the multisig signers and quorum updates are executed
+    /// @param updatesId The unique identifier for the updates
+    /// @param newSigners The new array of multisig signers
+    /// @param newQuorum The new required quorum for executing transfers
+    event MultiSigSignersAndQuorumUpdatesExecuted(uint256 indexed updatesId, address[] newSigners, uint256 newQuorum);
 
     /// @notice Reverts with an error if the number of signers is less than the quorum
     error SignersLengthCantBeLessThanQuorum();
@@ -76,6 +94,16 @@ contract MultiSign {
     error TransactionFailed();
     /// @notice Reverts with an error if the contract balance is zero
     error ContractBalanceIsZero();
+    /// @notice Reverts with an error if quorum and signers updates have already been approved by the signer
+    /// @param _updatesId The ID of the updates that have already been approved by signer
+    error UpdatesAlreadyApprovedBySigner(uint256 _updatesId);
+    /// @notice Reverts with an error if the updates have already been executed
+    /// @param _updatesId The ID of the updates that have already been executed
+    error UpdatesAlreadyExecuted(uint256 _updatesId);
+    /// @notice Reverts with an error if the quorum for updates is not reached
+    /// @param _needForUpdate The number of approvals needed for updates
+    /// @param _currentApprovals The current number of approvals for the updates
+    error QuorumNotReachedForUpdate(uint256 _needForUpdate, uint256 _currentApprovals);
 
     /// @dev The struct is used to store the details of a transfer
     /// @param to The address to which the transfer is made
@@ -91,10 +119,20 @@ contract MultiSign {
         mapping(address => bool) approvals;
     }
 
+    struct ChangeSignersAndQuorum {
+        address[] newSig;
+        uint256 newQuorum;
+        uint256 approvalCount;
+        bool executed;
+        mapping(address => bool) approvals;
+    }
+
     /// @notice A mapping from transfer ID to Transfer struct
     mapping(uint256 => Transfer) private transfers;
     /// @notice A mapping from address to boolean indicating whether the address is a signer
     mapping(address => bool) public isSigner;
+    /// @notice A mapping from ID to ChangeSignersAndQuorum struct
+    mapping(uint256 => ChangeSignersAndQuorum) public mapSignersAndQuorum;
 
     /// @notice Modifier to restrict access to only signers
     modifier onlySigner() {
@@ -154,6 +192,73 @@ contract MultiSign {
         emit TransferApproved(_transferId, msg.sender);
     }
 
+    /// @notice Function to initiate updates to the multisig signers and quorum
+    /// @param _newSignersArray An array of new multisig signers
+    /// @param _newQuorum The new required quorum for executing transfers
+    /// @return _updatesId The ID of the initiated updates
+    function initiateUpdateSignersAndQuorum(address[] memory _newSignersArray, uint256 _newQuorum)
+        external
+        onlySigner
+        returns (uint256 _updatesId)
+    {
+        if (_newSignersArray.length == 0) revert SignersLengthMustBeGreaterThanZero();
+        if (_newQuorum == 0) revert QuorumMustBeGreaterThanZero();
+        if (_newQuorum > _newSignersArray.length) revert SignersLengthCantBeLessThanQuorum();
+
+        _updatesId = signersAndQuorumChangesCount++;
+        ChangeSignersAndQuorum storage newData = mapSignersAndQuorum[_updatesId];
+
+        newData.newSig = _newSignersArray;
+        newData.newQuorum = _newQuorum;
+        newData.executed = false;
+        newData.approvalCount = 1;
+        newData.approvals[msg.sender] = true;
+
+        emit MultiSigSignersAndQuorumUpdatesInitiated(_updatesId, _newSignersArray, _newQuorum);
+    }
+
+    /// @notice Function to approve new multisig signers and quorum by a signer
+    /// @param _updatesId The ID of the updates to be approved
+    /// @dev This function can only be called by a signer
+    function approveNewSignersAndQuorum(uint256 _updatesId) external onlySigner {
+        ChangeSignersAndQuorum storage newData = mapSignersAndQuorum[_updatesId];
+
+        if (newData.executed) revert UpdatesAlreadyExecuted(_updatesId);
+        if (newData.approvals[msg.sender]) revert UpdatesAlreadyApprovedBySigner(_updatesId);
+
+        newData.approvalCount = newData.approvalCount + 1;
+        newData.approvals[msg.sender] = true;
+
+        emit MultiSigSignersAndQuorumUpdatesApproved(_updatesId, msg.sender);
+    }
+
+    /// @notice Execute updates the list of multisig signers and sets a new quorum
+    /// @dev This function can only be called by a signer and requires the approval of the quorum
+    /// @param _updatesId The ID of the updates to be executed
+    function executeSignersAndQuorumUpdates(uint256 _updatesId) external onlySigner {
+        ChangeSignersAndQuorum storage newData = mapSignersAndQuorum[_updatesId];
+
+        if (newData.executed) revert UpdatesAlreadyExecuted(_updatesId);
+        if (newData.approvalCount < quorum) revert QuorumNotReachedForUpdate(quorum, newData.approvalCount);
+
+        for (uint256 i = 0; i < currentMultiSigSigners.length; i++) {
+            isSigner[currentMultiSigSigners[i]] = false;
+        }
+
+        delete currentMultiSigSigners;
+
+        for (uint256 i = 0; i < (newData.newSig).length; i++) {
+            address signer = (newData.newSig)[i];
+            if (signer == address(0)) revert SignerAddressCannotBeZero();
+            isSigner[signer] = true;
+            currentMultiSigSigners.push(signer);
+        }
+
+        quorum = newData.newQuorum;
+        newData.executed = true;
+        emit MultiSigSignersAndQuorumUpdatesExecuted(_updatesId, newData.newSig, newData.newQuorum);
+    }
+
     /// @notice View function to get the details of a transfer
     /// @param _transferId The ID of the transfer to retrieve
     /// @return _to The address to which the transfer is made
@@ -172,6 +277,18 @@ contract MultiSign {
         return (transfer.to, transfer.amount, transfer.approvalCount, transfer.executed);
     }
 
+    function getUpdateSignersAndQuorumInfo(uint256 _updatesId)
+        external
+        view
+        returns (address[] memory _newSigners, uint256 _newQuorum, uint256 _approvalCount, bool _executed)
+    {
+        ChangeSignersAndQuorum storage newData = mapSignersAndQuorum[_updatesId];
+
+        if (newData.newSig.length == 0) revert InvalidTransferId();
+
+        return (newData.newSig, newData.newQuorum, newData.approvalCount, newData.executed);
+    }
+
     /// @notice View function to check if a signer has approved a transfer
     /// @param _signer The address of the signer
     /// @param _transferId The ID of the transfer to check
@@ -187,6 +304,12 @@ contract MultiSign {
     /// @return The total number of transfers initiated
     function getTransfersCount() external view returns (uint256) {
         return transfersCount;
+    }
+
+    /// @notice View function to get the current multisig signers
+    /// @return An array of current multisig signers addresses
+    function getCurrentMultiSigSigners() external view returns (address[] memory) {
+        return currentMultiSigSigners;
     }
 
     /// @notice Fallback function to receive Ether
--- test/MultiSignTest.t.sol
+++ test/MultiSignTest.t.sol
@@ -14,6 +14,14 @@ contract MultiSignTest is Test {
     address sig2 = vm.addr(2);
     address sig3 = vm.addr(3);
 
+    uint256 signersQuorumChangesCount;
+    address[] _newSignersArray;
+    uint256 _newQuorum = 3;
+
+    address nSig1 = vm.addr(7);
+    address nSig2 = vm.addr(8);
+    address nSig3 = vm.addr(9);
+
     address recepientAddress = vm.addr(777);
 
     function setUp() public {
@@ -75,9 +83,6 @@ contract MultiSignTest is Test {
         vm.prank(sig1);
         vm.expectRevert(MultiSign.AlreadyApprovedBySigner.selector);
         multis.approveTransfer(0);
-
-        // vm.expectEmit(true, true, false, false);
-        // emit MultiSign.TransferApproved(0, sig1);
 
         vm.prank(sig2);
         multis.approveTransfer(0);
@@ -157,6 +162,15 @@ contract MultiSignTest is Test {
         assertEq(afterFirstTransferInit, 1);
     }
 
+    function test_getCurrentSignersFunctionalityCheck() public {
+        vm.startPrank(sig1);
+        address[] memory currentSigners = multis.getCurrentMultiSigSigners();
+        assertEq(currentSigners.length, 3);
+        assertEq(currentSigners[0], sig1);
+        assertEq(currentSigners[1], sig2);
+        assertEq(currentSigners[2], sig3);
+    }
+
     function test_onlyMultisignerCheck() public {
         address randomAddress = vm.addr(777);
 
@@ -190,5 +204,129 @@ contract MultiSignTest is Test {
 
         vm.expectRevert(MultiSign.QuorumMustBeGreaterThanZero.selector);
         new MultiSign(signersArray, 0);
+    }
+
+    function test_initUpdateSignersAndQuorumRevertsZeroQuorumCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        vm.expectRevert(MultiSign.QuorumMustBeGreaterThanZero.selector);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, 0);
+    }
+
+    function test_initUpdateSignersAndQuorumRevertsZeroSignersCheck() public {
+        vm.startPrank(sig1);
+        vm.expectRevert(MultiSign.SignersLengthMustBeGreaterThanZero.selector);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+    }
+
+    function test_initUpdateSignersAndQuorumRevertsSignersVsQuorumLengthCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+
+        vm.startPrank(sig1);
+        vm.expectRevert(MultiSign.SignersLengthCantBeLessThanQuorum.selector);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, 3);
+    }
+
+    function test_initUpdateSignersAndQuorumFunctionalityCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+
+        vm.expectEmit(true, false, false, true);
+        emit MultiSign.MultiSigSignersAndQuorumUpdatesInitiated(0, _newSignersArray, _newQuorum);
+
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+        (address[] memory _newSigners,, uint256 _approvalCount, bool _executed) =
+            multis.getUpdateSignersAndQuorumInfo(0);
+        assertEq(_newSigners.length, _newQuorum);
+        assertEq(_approvalCount, 1);
+        assertEq(_executed, false);
+    }
+
+    function test_approveNewSignersAndQuorumRevertsIfAlreadyApproved() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+
+        vm.expectRevert(MultiSign.AlreadyApprovedBySigner.selector, 0);
+        multis.approveNewSignersAndQuorum(0);
+    }
+
+    function test_approveNewSignersAndQuorumFunctionalutyCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+
+        vm.startPrank(sig2);
+        vm.expectEmit(true, true, true, false);
+        emit MultiSign.MultiSigSignersAndQuorumUpdatesApproved(0, sig2);
+        multis.approveNewSignersAndQuorum(0);
+    }
+
+    function test_executeSignersAndQuorumUpdatesRevertsCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+
+        vm.startPrank(sig2);
+        (,, uint256 _approvalCount,) = multis.getUpdateSignersAndQuorumInfo(0);
+        vm.expectRevert(abi.encodeWithSelector(MultiSign.QuorumNotReachedForUpdate.selector, quorum, _approvalCount));
+        multis.executeSignersAndQuorumUpdates(0);
+        multis.approveNewSignersAndQuorum(0);
+
+        vm.startPrank(sig3);
+        multis.approveNewSignersAndQuorum(0);
+
+        vm.expectRevert(MultiSign.UpdatesAlreadyExecuted.selector, 0);
+        multis.executeSignersAndQuorumUpdates(0);
+    }
+
+    function test_executeSignersAndQuorumUpdatesRevertsIfEvenOneAddressZeroCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(address(0));
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+
+        vm.startPrank(sig2);
+        multis.approveNewSignersAndQuorum(0);
+        vm.expectRevert(MultiSign.SignerAddressCannotBeZero.selector);
+        multis.executeSignersAndQuorumUpdates(0);
+    }
+
+    function test_executeSignersAndQuorumUpdatesFunctionalityCheck() public {
+        _newSignersArray.push(nSig1);
+        _newSignersArray.push(nSig2);
+        _newSignersArray.push(nSig3);
+
+        vm.startPrank(sig1);
+        multis.initiateUpdateSignersAndQuorum(_newSignersArray, _newQuorum);
+
+        vm.startPrank(sig2);
+        multis.approveNewSignersAndQuorum(0);
+
+        multis.executeSignersAndQuorumUpdates(0);
+        (address[] memory _newSigners, uint256 _newQuorumData, uint256 _approvalCount, bool _executed) =
+            multis.getUpdateSignersAndQuorumInfo(0);
+        assertEq(_newSigners.length, 3);
+        assertEq(_newQuorumData, 3);
+        assertEq(_approvalCount, 2);
+        assertEq(_executed, true);
     }
 }
